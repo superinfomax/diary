26e986fe27c1bb62eeb624b42a9e096d1e0b274f
superinfomax <ansy959@gmail.com>
2024-06-08 22:02:49 +0800

    love

--- JourneySpace/Setting_1.swift
+++ JourneySpace/Setting_1.swift
@@ -4,28 +4,84 @@
 //
 //  Created by max on 2024/6/8.
 //
-
 import SwiftUI
-import Foundation
 
 struct SettingPage1: View {
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading) {
-                HStack(alignment: .top) {
-                    Text("Setting")
-                        .font(.custom("KOHO", size: 96))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
+        VStack {
+            VStack(alignment: .center, spacing: 16) {
+                Image("SumoneBasic")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                Text("相愛的 第 71 天")
+                    .font(.system(size: 18))
+                Text("大統領 ❤️ 小統領")
+                    .font(.system(size: 18))
+                Text("我們的小寶夢\n小寶夢")
+                    .font(.system(size: 14))
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            
+            HStack(spacing: 16) {
+                SettingButton(title: "便條紙", imageName: "folder.fill")
+                SettingButton(title: "道具背包", imageName: "backpack.fill")
+                SettingButton(title: "公告", imageName: "megaphone.fill")
+            }
+            .padding(.horizontal)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                NavigationLink(destination: Text("常見問題的問題")) {
+                    SettingRow(title: "經常問的問題")
+                }
+                NavigationLink(destination: Text("詢問想問團隊")) {
+                    SettingRow(title: "詢問想問團隊")
+                }
+                NavigationLink(destination: Text("我的想問資訊")) {
+                    SettingRow(title: "我的想問資訊")
+                }
+                NavigationLink(destination: Text("拜訪小寶夢IG")) {
+                    SettingRow(title: "拜訪小寶夢IG")
+                }
+                NavigationLink(destination: Text("想問的問題郵箱")) {
+                    SettingRow(title: "想問的問題郵箱")
                 }
             }
-
+            Spacer()
         }
+        .background(Color(UIColor.systemGray6))
+        .ignoresSafeArea()
+    }
+}
 
-          .frame(width: 430, height: 932)
-          .background(
-            Color(red: 71/255, green: 114/255, blue: 186/255)
-          )
+struct SettingButton: View {
+    let title: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: imageName)
+                .font(.system(size: 36))
+            Text(title)
+                .font(.system(size: 16))
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
 
-      }
+struct SettingRow: View {
+    let title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .foregroundColor(.black)
+            Spacer()
+            Image(systemName: "chevron.right")
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(8)
+    }
 }
