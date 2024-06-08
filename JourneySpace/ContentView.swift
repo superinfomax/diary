42e916072929863fd9aa119be49901e7f49cdb9b
JessieChiou <u11016014@go.utaipei.edu.tw>
2024-06-08 23:29:10 +0800

    finally adjust the ui

--- JourneySpace/ContentView.swift
+++ JourneySpace/ContentView.swift
@@ -108,3 +108,8 @@ struct ContentView: View {
         }
     }
 }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- JourneySpace/ToDo_list_1.swift
+++ JourneySpace/ToDo_list_1.swift
@@ -10,22 +10,37 @@ import Foundation
 
 struct TodoPage1: View {
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading) {
-                HStack(alignment: .top) {
+        HStack{
+            VStack(alignment: .leading, spacing: 15) {
+                HStack(alignment: .top, spacing: 15) {
                     Text("ToDo")
                         .font(.custom("KOHO", size: 96))
                         .fontWeight(.bold)
                         .foregroundColor(.white)
-                }
+                    VStack{
+                        Spacer()
+                        Image(systemName: "plus.circle")
+                            .resizable()
+                            .frame(width: 32.0, height: 32.0)
+                            .foregroundColor(.white)
+                    }
+                    .frame(height:90)
+                    Spacer()
             }
-
+                .frame(width: 350)
+                Spacer()
         }
-
+            .frame(height: 650)
+    }
           .frame(width: 430, height: 932)
           .background(
             Color(red: 71/255, green: 114/255, blue: 186/255)
           )
-
       }
+}
+
+struct ToDo_List_1_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoPage1()
+    }
 }
