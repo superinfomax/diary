afd32b62a48cd6b793cdd28e248916c4be743182
superinfomax <ansy959@gmail.com>
2024-06-14 22:02:56 +0800

    更新FloatingImageView的 動畫效果

--- JourneySpace/FirstPage.swift
+++ JourneySpace/FirstPage.swift
@@ -11,16 +11,15 @@ struct FloatingImage: View {
     let imageSize: CGSize
     @State private var rotationAngle: Double = 0
     @State private var position: CGPoint
-    @State private var velocity: CGPoint = CGPoint(x: 0.5, y: 0.5)
+    @State private var velocity: CGPoint = CGPoint(x: 0.3, y: 0.3)
     @State private var isDragging = false
-    @State private var isPaused = false
 
     let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
 
     init(imageName: String, imageSize: CGSize, initialPosition: CGPoint) {
         self.imageName = imageName
         self.imageSize = imageSize
-        self.position = initialPosition
+        self._position = State(initialValue: initialPosition)
     }
 
     var body: some View {
@@ -41,53 +40,35 @@ struct FloatingImage: View {
                     }
             )
             .onReceive(timer) { _ in
-                guard !isDragging && !isPaused else { return }
+                guard !isDragging else { return }
 
-                // 更新圖片位置
+                // 更新图片位置
                 position.x += velocity.x
                 position.y += velocity.y
-                
-                // 檢查邊界
-                if position.x > 430 {
-                    position.x = 430
-                    isPaused = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        position.x = 0
-                        isPaused = false
-                    }
-                } else if position.x < 0 {
-                    position.x = 0
-                    isPaused = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        position.x = 430
-                        isPaused = false
-                    }
+
+                // 检查边界并平滑处理
+                if position.x > 430 - imageSize.width / 2 {
+                    position.x = 430 - imageSize.width / 2
+                    velocity.x = -velocity.x * 0.7 // 减少速度，模拟更弱的反弹
+                } else if position.x < imageSize.width / 2 {
+                    position.x = imageSize.width / 2
+                    velocity.x = -velocity.x * 0.7
                 }
 
-                if position.y > 932 {
-                    position.y = 932
-                    isPaused = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        position.y = 0
-                        isPaused = false
-                    }
-                } else if position.y < 0 {
-                    position.y = 0
-                    isPaused = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                        position.y = 932
-                        isPaused = false
-                    }
+                if position.y > 932 - imageSize.height / 2 {
+                    position.y = 932 - imageSize.height / 2
+                    velocity.y = -velocity.y * 0.7
+                } else if position.y < imageSize.height / 2 {
+                    position.y = imageSize.height / 2
+                    velocity.y = -velocity.y * 0.7
                 }
 
-                // 隨機變換速度方向
-                if Bool.random() {
-                    velocity.x = CGFloat.random(in: 0...0.3)
-                    velocity.y = CGFloat.random(in: 0...0.3)
-                }
+                // 随机变换速度方向
+                velocity.x += CGFloat.random(in: -0.05...0.05)
+                velocity.y += CGFloat.random(in: -0.05...0.05)
             }
             .onAppear {
-                // 設置自轉動畫
+                // 设置自转动画
                 withAnimation(Animation.linear(duration: 10).repeatForever(autoreverses: false)) {
                     self.rotationAngle = 360
                 }
@@ -98,10 +79,9 @@ struct FloatingImage: View {
 struct FirstPage1: View {
     var body: some View {
         ZStack {
-            FloatingImage(imageName: "love", imageSize: CGSize(width: 100, height: 100), initialPosition: CGPoint(x: CGFloat.random(in: 0...400), y: CGFloat.random(in: 0...900)))
-            FloatingImage(imageName: "angry", imageSize: CGSize(width: 150, height: 150), initialPosition: CGPoint(x: CGFloat.random(in: 0...400), y: CGFloat.random(in: 0...900)))
-            FloatingImage(imageName: "happy", imageSize: CGSize(width: 150, height: 150), initialPosition: CGPoint(x: CGFloat.random(in: 0...400), y: CGFloat.random(in: 0...900)))
-
+            FloatingImage(imageName: "love", imageSize: CGSize(width: 100, height: 100), initialPosition: CGPoint(x: CGFloat.random(in: 50...350), y: CGFloat.random(in: 50...850)))
+            FloatingImage(imageName: "angry", imageSize: CGSize(width: 150, height: 150), initialPosition: CGPoint(x: CGFloat.random(in: 75...325), y: CGFloat.random(in: 75...825)))
+            FloatingImage(imageName: "happy", imageSize: CGSize(width: 150, height: 150), initialPosition: CGPoint(x: CGFloat.random(in: 75...325), y: CGFloat.random(in: 75...825)))
         }
         .frame(width: 400, height: 932)
         .background(
