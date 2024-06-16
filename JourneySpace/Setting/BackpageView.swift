d6f76a7cd23c1df4a6593f14ae38df9a4e4c92bd
superinfomax <ansy959@gmail.com>
2024-06-16 17:38:34 +0800

    SettingPage1 抽獎動畫以及獎品新增

--- JourneySpace/Setting/BackpageView.swift
+++ JourneySpace/Setting/BackpageView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 import Foundation
 
 struct BackpageView: View {
+    let prizeImage: String?
+    
     var body: some View {
-        Image(systemName: "backpack.fill")
+        VStack {
+            if let prizeImage = prizeImage {
+                Image(prizeImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .padding()
+                Text("This is your prize!")
+                    .font(.title)
+                    .padding()
+            } else {
+                Text("No prize won yet.")
+                    .font(.title)
+                    .padding()
+            }
+        }
+        .navigationTitle("Your Prize")
     }
 }
