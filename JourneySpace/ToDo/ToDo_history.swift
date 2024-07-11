8754405723c906f888a98c9452ba6afc8028fde3
Roi8648 <u11016013@go.utaipei.edu.tw>
2024-07-11 17:16:41 +0800

    new UI for todo_history!

--- JourneySpace/ToDo/ToDo_history.swift
+++ JourneySpace/ToDo/ToDo_history.swift
@@ -20,87 +20,112 @@ struct ToDo_history: View {
         order: .forward
     ) private var items: [ToDoItem]
     
+    @State private var currentImage: String = "eatTrashYelo"
+    
     var body: some View {
-        List {
-            ForEach(sortedItems) { item in
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("\(item.timestamp, format: Date.FormatStyle(date: .abbreviated))")
-                            .font(.caption2)
-                            .foregroundColor(.gray)
-                            
-                        if item.isCritical {
-                            Image(systemName: "exclamationmark.2")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.red)
+        ZStack {
+            Image("todo_HistoryBackground")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                List {
+                    ForEach(sortedItems) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("\(item.timestamp, format: Date.FormatStyle(date: .abbreviated))")
+                                    .font(.caption2)
+                                    .foregroundColor(.gray)
+                                
+                                if item.isCritical {
+                                    Image(systemName: "exclamationmark.2")
+                                        .symbolVariant(.fill)
+                                        .foregroundColor(.red)
+                                        .font(.title)
+                                        .bold()
+                                        .padding(.trailing)
+                                }
+                                
+                                Text(item.title)
+                                    .font(.title)
+                                    .bold()
+                                    .foregroundColor(.black)
+                                
+                            }
+                            Spacer()
+                            Text("\(item.timestamp, format: Date.FormatStyle(time: .shortened))")
                                 .font(.title)
                                 .bold()
-                                .padding(.trailing)
+                                .foregroundColor(.black)
+                            //                                    .padding(.trailing, 10)
+                                .padding()
+                            //                                Button {
+                            //                                    withAnimation {
+                            //                                        item.isCompleted.toggle()
+                            //                                    }
+                            //                                } label: {
+                            //                                    Image(systemName: "checkmark")
+                            //                                        .symbolVariant(.circle.fill)
+                            //                                        .foregroundStyle(item.isCompleted ? .green : .gray)
+                            //                                        .font(.largeTitle)
+                            //                                }
+                            //                                .buttonStyle(.plain)
                         }
-                        
-                        Text(item.title)
-                            .font(.title)
-                            .bold()
-                            .foregroundColor(.black)
-                        
-                    }
-                    Spacer()
-                    Text("\(item.timestamp, format: Date.FormatStyle(time: .shortened))")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.black)
-//                                    .padding(.trailing, 10)
+                        .frame(width: 340)
                         .padding()
-//                                Button {
-//                                    withAnimation {
-//                                        item.isCompleted.toggle()
-//                                    }
-//                                } label: {
-//                                    Image(systemName: "checkmark")
-//                                        .symbolVariant(.circle.fill)
-//                                        .foregroundStyle(item.isCompleted ? .green : .gray)
-//                                        .font(.largeTitle)
-//                                }
-//                                .buttonStyle(.plain)
-                }
-                .frame(width: 340)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(10)
-                .shadow(radius: 3)
-
-
-                .swipeActions(edge: .leading) {
-                    Button (role: .destructive){
-                        withAnimation {
-                            item.isCompleted.toggle()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 3)
+                        
+                        
+                        .swipeActions(edge: .leading) {
+                            Button (role: .destructive){
+                                withAnimation {
+                                    item.isCompleted.toggle()
+                                }
+                            } label: {
+                                Image(uiImage: UIImage(systemName: "arrowshape.turn.up.backward.badge.clock.fill")!.withTintColor(.gray, renderingMode: .alwaysOriginal))
+                            }
+                            .tint(.clear)
                         }
-                    } label: {
-                        Image(uiImage: UIImage(systemName: "arrowshape.turn.up.backward.badge.clock.fill")!.withTintColor(.gray, renderingMode: .alwaysOriginal))
-                        }
-                        .tint(.clear)
+                        
                     }
+                    //            .offset(y:20)
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(Color.clear)
+                    // padding 用來調整todo之間的間距
+                    .padding(.vertical, -2)
                     
-            }
-//            .offset(y:20)
-            .listRowSeparator(.hidden)
-            .listRowBackground(Color.clear)
-            // padding 用來調整todo之間的間距
-            .padding(.vertical, -3)
-
-            
-        }
-        .listStyle(PlainListStyle())
-        .background(Color.clear)
-    }
-    private var sortedItems: [ToDoItem] {
-            items.sorted {
-                if $0.isCritical != $1.isCritical {
-                    return $0.isCritical && !$1.isCritical
+                    
                 }
-                return $0.timestamp < $1.timestamp
+                .listStyle(PlainListStyle())
+                
+                Spacer()
+                
+                Image(currentImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .padding()
+//                    .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height - 200)
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.6) {
+                    currentImage = "confuseYelo"
+                }
             }
         }
+    }
+    
+    private var sortedItems: [ToDoItem] {
+        items.sorted {
+            if $0.isCritical != $1.isCritical {
+                return $0.isCritical && !$1.isCritical
+            }
+            return $0.timestamp < $1.timestamp
+        }
+    }
 }
 
 #Preview {
