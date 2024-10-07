8dce77451422926ae69acea2ecf946c1e49c3fb4
superinfomax <ansy959@gmail.com>
2024-10-07 22:06:25 +0800

    修好 SettingView 組員看不到的問題

--- JourneySpace/Setting/SettingsView.swift
+++ JourneySpace/Setting/SettingsView.swift
@@ -30,118 +30,107 @@ struct SettingsView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading, spacing: 1) {
-//                HStack {
-//                    Button(action: {
-//                        presentationMode.wrappedValue.dismiss()
-//                    }) {
-//                        Image(systemName: "arrow.left")
-//                            .font(.system(size: 24))
-//                            .padding()
-//                            .foregroundColor(.white)
-//                    }
-//                }
-//                .padding(.horizontal)
-                
-                
-                Form {
-                    Section(header: Text("系統設定")) {
-                        Toggle(isOn: $isScreenLockOn) {
-                            Text("螢幕鎖定")
-                        }
-                        .foregroundColor(.white)
-                        .onChange(of: isScreenLockOn) { value in
-                            if value {
-                                authenticate()
-                            }
-                        }
-                        
-                        Toggle(isOn: $isNotificationOn) {
-                            Text("通知")
-                        }
-                        .foregroundColor(.white)
-                        .onChange(of: isNotificationOn) { value in
-                            if value {
-                                requestNotificationPermission()
-                            }
-                        }
-                        
-                        NavigationLink(destination: Text("語言設定")) {
-                            SettingRow1(title: "語言設定", imageName: "translate")
-                        }
-                        .foregroundColor(.white)
-                        
-                        NavigationLink(destination: Text("可以設定幾點要通知使用者寫日記")) {
-                            SettingRow1(title: "通知設定", imageName: "bell")
-                        }
-                        .foregroundColor(.white)
-                    }
-                    .foregroundColor(.gray)
-                    .listRowBackground(Color(red: 144/255, green: 132/255, blue: 204/255))
-                    
-                    Section(header: Text("有關團隊")) {
-                        NavigationLink(destination: TeamMemberView()) {
-                            SettingRow1(title: "團隊成員", imageName: "person.2")
-                        }
-                        .foregroundColor(.white)
-                        NavigationLink(destination:
-                                        Text("開發的 第 \(daysSinceDevelop()) 天")
-                            .font(.system(size: 18))
-                            .multilineTextAlignment(.center)
-                            .padding()) {
-                                SettingRow1(title: "開發狀況", imageName: "wrench.and.screwdriver")
+        NavigationView { // Added NavigationView
+            ZStack {
+                VStack(alignment: .leading, spacing: 1) {
+                    Form {
+                        Section(header: Text("系統設定")) {
+                            Toggle(isOn: $isScreenLockOn) {
+                                Text("螢幕鎖定")
                             }
                             .foregroundColor(.white)
+                            .onChange(of: isScreenLockOn) { value in
+                                if value {
+                                    authenticate()
+                                }
+                            }
+                            
+                            Toggle(isOn: $isNotificationOn) {
+                                Text("通知")
+                            }
+                            .foregroundColor(.white)
+                            .onChange(of: isNotificationOn) { value in
+                                if value {
+                                    requestNotificationPermission()
+                                }
+                            }
+                            
+                            NavigationLink(destination: Text("語言設定")) {
+                                SettingRow1(title: "語言設定", imageName: "translate")
+                            }
+                            .foregroundColor(.white)
+                            
+                            NavigationLink(destination: Text("可以設定幾點要通知使用者寫日記")) {
+                                SettingRow1(title: "通知設定", imageName: "bell")
+                            }
+                            .foregroundColor(.white)
+                        }
+                        .foregroundColor(.gray)
+                        .listRowBackground(Color(red: 144/255, green: 132/255, blue: 204/255))
                         
-                        NavigationLink(destination: Text("我愛東華")) {
-                            SettingRow1(title: "想問的資訊", imageName: "questionmark.circle")
+                        Section(header: Text("有關團隊")) {
+                            NavigationLink(destination: TeamMemberView()) {
+                                SettingRow1(title: "團隊成員", imageName: "person.2")
+                            }
+                            .foregroundColor(.white)
+                            
+                            NavigationLink(destination:
+                                            Text("開發的 第 \(daysSinceDevelop()) 天")
+                                .font(.system(size: 18))
+                                .multilineTextAlignment(.center)
+                                .padding()) {
+                                    SettingRow1(title: "開發狀況", imageName: "wrench.and.screwdriver")
+                                }
+                                .foregroundColor(.white)
+                            
+                            NavigationLink(destination: Text("我愛東華")) {
+                                SettingRow1(title: "想問的資訊", imageName: "questionmark.circle")
+                            }
+                            .foregroundColor(.white)
+                            
+                            NavigationLink(destination: Image("senbei1").scaledToFit()) {
+                                SettingRow1(title: "拜訪煎餅的IG", imageName: "camera")
+                            }
+                            .foregroundColor(.white)
+                            
+                            NavigationLink(destination: Text("垃圾郵箱")) {
+                                SettingRow1(title: "問題郵箱", imageName: "trash")
+                            }
+                            .foregroundColor(.white)
                         }
-                        .foregroundColor(.white)
-                        NavigationLink(destination: Image("senbei1").scaledToFit) {
-                            SettingRow1(title: "拜訪煎餅的IG", imageName: "camera")
-                        }
-                        .foregroundColor(.white)
-                        NavigationLink(destination: Text("垃圾郵箱")) {
-                            SettingRow1(title: "問題郵箱", imageName: "trash")
-                        }
-                        .foregroundColor(.white)
-                    }
-                    .foregroundColor(.gray)
-                    .listRowBackground(Color(red: 144/255, green: 132/255, blue: 204/255))
-                    
-                    Section {
-                        NavigationLink(destination: Text("個人APP連結")) {
-                            SettingRow1(title: "個人", imageName: "person.crop.circle")
-                        }
+                        .foregroundColor(.gray)
+                        .listRowBackground(Color(red: 144/255, green: 132/255, blue: 204/255))
                         
-                        NavigationLink(destination: Text("登出")) {
-                            SettingRow1(title: "登出", imageName: "arrowshape.turn.up.left")
+                        Section {
+                            NavigationLink(destination: Text("個人APP連結")) {
+                                SettingRow1(title: "個人", imageName: "person.crop.circle")
+                            }
+                            
+                            NavigationLink(destination: Text("登出")) {
+                                SettingRow1(title: "登出", imageName: "arrowshape.turn.up.left")
+                            }
+                        }
+                        .foregroundColor(.white)
+                        .listRowBackground(Color(red: 144/255, green: 132/255, blue: 204/255))
+                    }
+                    .onAppear {
+                        if isScreenLockOn {
+                            authenticate()
                         }
                     }
-                    .foregroundColor(.white)
-                    .listRowBackground(Color(red: 144/255, green: 132/255, blue: 204/255))
-                }
-                .onAppear {
-                    if isScreenLockOn {
-                        authenticate()
+                    .alert(isPresented: $showingAuth) {
+                        Alert(title: Text("認證失敗"), message: Text("無法進行身份驗證，請重試。"), dismissButton: .default(Text("確定")))
                     }
+                    .alert(isPresented: $showingNotificationError) {
+                        Alert(title: Text("通知失敗"), message: Text("無法開啟通知，請檢查您的設定。"), dismissButton: .default(Text("確定")))
+                    }
+                    .tint(Color(red: 34/255, green: 40/255, blue: 64/255))
+                    .background(Color(red: 34/255, green: 40/255, blue: 64/255))
+                    .scrollContentBackground(.hidden)
                 }
-                .navigationBarHidden(true)
-                .alert(isPresented: $showingAuth) {
-                    Alert(title: Text("認證失敗"), message: Text("無法進行身份驗證，請重試。"), dismissButton: .default(Text("確定")))
-                }
-                .alert(isPresented: $showingNotificationError) {
-                    Alert(title: Text("通知失敗"), message: Text("無法開啟通知，請檢查您的設定。"), dismissButton: .default(Text("確定")))
-                }
-                .tint(Color(red: 34/255, green: 40/255, blue: 64/255))
-                .background(Color(red: 34/255, green: 40/255, blue: 64/255))
-                .scrollContentBackground(.hidden)
-                
             }
+            .background(Color(red: 34/255, green: 40/255, blue: 64/255))
         }
-        // for navigationBar color
-        .background(Color(red: 34/255, green: 40/255, blue: 64/255))
     }
     
     func authenticate() {
@@ -199,6 +188,7 @@ struct SettingsView: View {
         }
     }
 }
+
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
