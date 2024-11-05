c5edd840ce1a2e7380b7bb9079aef3a512ccbe5d
JessieChiou <u11016014@go.utaipei.edu.tw>
2024-11-06 01:57:29 +0800

    第一次開啟app時會問是否開啟通知

--- JourneySpace/JourneySpaceApp.swift
+++ JourneySpace/JourneySpaceApp.swift
@@ -8,12 +8,38 @@
 import SwiftUI
 import Foundation
 import SwiftData
+import UIKit
+
 @main
 struct JourneySpaceApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
                 .modelContainer(for: ToDoItem.self)
         }
+    }
+}
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        registerForNotification()
+        return true
+    }
+    
+    
+    func registerForNotification() {
+        //For device token and push notifications.
+        UIApplication.shared.registerForRemoteNotifications()
+        
+        let center : UNUserNotificationCenter = UNUserNotificationCenter.current()
+        //        center.delegate = self
+        
+        center.requestAuthorization(options: [.sound , .alert , .badge ], completionHandler: { (granted, error) in
+            if ((error != nil)) { UIApplication.shared.registerForRemoteNotifications() }
+            else {
+                
+            }
+        })
     }
 }
